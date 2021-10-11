124ffd9a58d2abf3a136175c8f88940ccee2e651
Michal <michal.seifert@wp.pl>
2021-10-12 00:50:14 +0200

    -new project

---
+++ app/src/main/java/com/example/pracainzynierska/loginPage/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.pracainzynierska.loginPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.pracainzynierska.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
